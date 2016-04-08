8e70adccb6aa5e56759ecb711b093e2b85c498fd
Nick Dodson <silentcicero@outlook.com>
2016-04-08 13:40:24 -0400

    add `AttributesSet` event
    Properties:
    
    _sender: the message sender
    _timestamp: when the attribute was set
    
    =D

--- contracts/UportRegistry.sol
+++ contracts/UportRegistry.sol
@@ -1,4 +1,5 @@
 contract UportRegistry {
+  event AttributesSet(address indexed _sender, uint _timestamp);
 
   uint public version;
   address public previousPublishedVersion;
@@ -12,6 +13,7 @@ contract UportRegistry {
 
   function setAttributes(bytes ipfsHash) {
     ipfsAttributeLookup[msg.sender] = ipfsHash;
+    AttributesSet(msg.sender, now);
   }
 
   function getAttributes(address personaAddress) constant returns(bytes) {
