0ea8be42e6d404a6ecd715a7608ee85b914f1b5d
haad <haad@headbanggames.com>
2016-08-18 12:34:44 +0200

    Refactor: Start web3 and ipfs providers (testrpc and ipfs daemons) before running tests.

--- test/uportregistry.js
+++ test/uportregistry.js
@@ -1,23 +1,27 @@
-var assert          = require('chai').assert;
-var Web3            = require('web3');
-var web3            = new Web3();
-var web3prov        = new web3.providers.HttpProvider('http://localhost:8545');
-web3.setProvider(web3prov);
-
-var pudding         = require('ether-pudding')
-pudding.setWeb3(web3);
+var assert    = require('chai').assert;
+var Web3      = require('web3');
+var web3      = new Web3();
+var pudding   = require('ether-pudding');
+var testUtils = require('./testutils');
 
 var UportRegistry = require("../environments/development/contracts/UportRegistry.sol.js").load(pudding);
-UportRegistry = pudding.whisk({binary: UportRegistry.binary, abi: UportRegistry.abi})
+UportRegistry = pudding.whisk({binary: UportRegistry.binary, abi: UportRegistry.abi});
 
 describe('UportRegistry', function() {
+  this.timeout(10000);
+
+  before(function (done) {
+    testUtils.startProviders(function (err, res) {
+      // pudding.setWeb3(web3); // doesn't seem to be needed
+      web3.setProvider(res.web3Provider);
+      done();
+    });
+  });
 
   var ipfsHash = '0x1220aaabbbcccdddeeefff00011122233344';
   var ipfsHash2 = '0x1220';
 
-  it("should create a new registry and set attributes correctly", function(done) {
-    this.timeout(10000);
-
+  it("should create a new registry and set attributes correctly", function (done) {
     web3.eth.getAccounts(function (err, acct) {
 
       var previousPublished = acct[2];
@@ -25,16 +29,16 @@ describe('UportRegistry', function() {
       UportRegistry.new(previousPublished, {from:acct[0]}).then(function (reg) {
         reg.setAttributes(ipfsHash, {from:acct[0]}).then(function () {
           return reg.getAttributes.call(acct[0]);
-        }).then(function(returnedBytes) {
+        }).then(function (returnedBytes) {
           assert.strictEqual(returnedBytes, ipfsHash);
-          reg.setAttributes(ipfsHash2, {from: acct[1]})
-        }).then(function() {
+          reg.setAttributes(ipfsHash2, {from: acct[1]});
+        }).then(function () {
           return reg.getAttributes.call(acct[1]);
-        }).then(function(returnedBytes2) {
+        }).then(function (returnedBytes2) {
           assert.strictEqual(returnedBytes2, ipfsHash2);
           done();
         }).catch(done);
-      })
-    })
-  })
+      });
+    });
+  });
 });
