1c606123a9c074d9637ce82cf75dd6f7f5d362e5
Zac Mitton <zacmitton22@gmail.com>
2017-02-03 11:11:53 -0500

    bare bones interface for new registry

--- contracts/UportRegistry.sol
+++ contracts/UportRegistry.sol
@@ -6,22 +6,23 @@ contract UportRegistry{
   mapping(bytes32 => mapping(address => mapping(address => bytes32))) public registry;
 
   function UportRegistry(address _previousPublishedVersion) {
-    version = 2;
+    version = 3;
     previousPublishedVersion = _previousPublishedVersion;
   }
 
   event Set(
     bytes32 indexed registrationIdentifier,
-    address indexed attestor,
-    address indexed attestee);
+    address indexed issuer,
+    address indexed subject,
+    uint updatedAt);
 
   //create or update
-  function set(bytes32 registrationIdentifier, address attestee, bytes32 value){
-      Set(registrationIdentifier, msg.sender, attestee);
-      registry[registrationIdentifier][msg.sender][attestee] = value;
+  function set(bytes32 registrationIdentifier, address subject, bytes32 value){
+      Set(registrationIdentifier, msg.sender, subject, now);
+      registry[registrationIdentifier][msg.sender][subject] = value;
   }
 
-  function get(bytes32 registrationIdentifier, address attestor, address attestee) constant returns(bytes32){
-      return registry[registrationIdentifier][attestor][attestee];
+  function get(bytes32 registrationIdentifier, address issuer, address subject) constant returns(bytes32){
+      return registry[registrationIdentifier][issuer][subject];
   }
 }
