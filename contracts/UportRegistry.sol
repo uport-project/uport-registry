4be269dfc9246d42fca4052aade35c9886e88350
Nikolai Mushegian <nikolai@nexusdev.us>
2017-04-02 11:02:06 -0400

    extra index, improve readability, add license

--- contracts/UportRegistry.sol
+++ contracts/UportRegistry.sol
@@ -1,30 +1,48 @@
+// Copyright (C) 2016, 2017  ConsenSys, Inc
+// Copyright (C) 2017        DappHub, LLC
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 pragma solidity 0.4.8;
 
 contract UportRegistry {
   uint public version;
-  address public previousPublishedVersion;
-  mapping(address => mapping(address => mapping(bytes32 => bytes32))) public registry;
+  address public lastVersion;
+  mapping(address=>
+    mapping(address=>
+      mapping(bytes32=>
+        bytes32))) public registry;
 
-  function UportRegistry(address _previousPublishedVersion) {
+  function UportRegistry(address _lastVersion) {
     version = 3;
-    previousPublishedVersion = _previousPublishedVersion;
+    lastVersion = _lastVersion;
   }
 
   event Set (
-    address indexed from,
-    address indexed to,
+    address indexed src,
+    address indexed dst,
     bytes32 indexed key,
-    bytes32         value,
-    uint256         updatedAt
-  );
+    bytes32 indexed val,
+    uint256         tau
+  ) anonymous;
 
   //create or update
-  function set(address to, bytes32 key, bytes32 value) {
-      Set(msg.sender, to, key, value, now);
-      registry[msg.sender][to][key] = value;
+  function set(address dst, bytes32 key, bytes32 val) {
+      Set(msg.sender, dst, key, val, now);
+      registry[msg.sender][dst][key] = val;
   }
 
-  function get(address from, address to, bytes32 key) constant returns(bytes32) {
-      return registry[from][to][key];
+  function get(address src, address dst, bytes32 key) constant returns (bytes32 val) {
+      return registry[src][dst][key];
   }
 }
